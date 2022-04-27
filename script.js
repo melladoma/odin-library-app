f90e24bc35119abde47058f2216e4d12a8a01a56
Marine Mellado <marine.mellado@gmail.com>
2022-04-27 09:25:05 +0200

    delete buttons feature fixed+CSS mock-up tries

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-let myLibrary = [];
+
 let myLibraryStored;
-let storedArray;
+const bookContainer = document.getElementById('book-container');
+
+const formFields = document.querySelector('form').querySelectorAll('input');
+
+
+
+function addBookToLibrary() {
+    let myLibraryTemp = [];
+    let userInput = [];
+    //take user's input and store the new book objects into array
+    formFields.forEach(field => {
+        userInput.push(field.value)
+    });
+    let book = new Book(userInput);
+    myLibraryTemp.push(book);
+    sendToLocalStorage(myLibraryTemp);
+
+    //form visual clearance
+    formFields.forEach(field => {
+        field.value = "";
+    });
+    userInput = [];
+    //displays new book + stored books
+    displayBooks();
+}
+
 
 function Book(inputArray) {
     //constructor function
@@ -11,6 +36,7 @@ function Book(inputArray) {
 }
 
 function retrieveLocalStorage() {
+    let storedArray;
     storedArray = localStorage.getItem("myLibraryStored");
     myLibraryStored = JSON.parse(storedArray);
 }
@@ -21,39 +47,61 @@ function sendToLocalStorage(newArray) {
     localStorage.setItem("myLibraryStored", JSON.stringify(myLibraryStored));
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 function displayBooks() {
+    removeAllChildNodes(bookContainer);
     if (localStorage.getItem("myLibraryStored") != undefined) {
         retrieveLocalStorage();
-        myLibraryStored.forEach(item => {
-            console.log(item)
-        })
     }
+    myLibraryStored.forEach(book => {
+        let card = document.createElement('div');
+        let buttonRemove = document.createElement('button');
+        buttonRemove.innerText = "Delete";
+        buttonRemove.classList.add('button-remove');
+        buttonRemove.addEventListener('click', deleteBook);
+
+        for (let item in book) {
+            let line = document.createElement('div');
+            if (item === "numPages") {
+                line.innerText = `${book[item]} pages`;
+            } else if (item === "readStatus") {
+                if (book[item] === "yes") {
+                    line.innerText = "read";
+                } else if (book[item] === "no") {
+                    line.innerText = "not read yet";
+                }
+            } else {
+                line.innerText = book[item];
+            }
+            let bookIndex = myLibraryStored.indexOf(book);
+            card.setAttribute('data-key', bookIndex);
+            card.setAttribute('class', 'card');
+            card.appendChild(line);
+            card.appendChild(buttonRemove);
+
+        }
+        bookContainer.appendChild(card)
+    })
+}
+
+function deleteBook(ev) {
+    const cardToDelete = ev.currentTarget.parentNode;
+    bookContainer.removeChild(cardToDelete);
+    let key = cardToDelete.getAttribute('data-key');
+    myLibraryStored.splice(key, 1);
+    localStorage.setItem("myLibraryStored", JSON.stringify(myLibraryStored));
 }
 
 displayBooks();
 
 const submitButton = document.getElementById('submitForm');
-const formFields = document.querySelector('form').querySelectorAll('input');
+submitButton.addEventListener('click', addBookToLibrary);
 
-submitButton.addEventListener('click', addBookToLibrary)
-let userInput = [];
-function addBookToLibrary() {
-    //take user's input and store the new book objects into array
-
-    formFields.forEach(field => {
-        userInput.push(field.value)
-        console.log(field.value)
-    });
-    let book = new Book(userInput);
-    myLibrary.push(book)
-    sendToLocalStorage(myLibrary);
-
-    formFields.forEach(field => {
-        field.value = "";
-    });
-    userInput = [];
-
-
-    displayBooks();
-}
-
+//to trouble shoot :
+//double card issued? 
+//add regEx sur yes/no if not input blocked
